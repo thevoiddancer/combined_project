aeeaa89153ba7002f4a7a6af3e687be5c23db2d1
Caskey5 <siljacantonio@gmail.com>
2025-04-28 21:03:37 +0200

    Obrisan folder 'kozmeticki salon' zbog izmjena

--- kozmeticki salon/main.py
+++
@@ -1,4 +0,0 @@
-from test import start_app
-
-if __name__ == "__main__":
-    start_app()
--- kozmeticki salon/validation/validators.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def is_valid_password(password):
-    # Bar jedno veliko slovo, broj i poseban znak
-    if (re.search(r"[A-Z]", password) and
-        re.search(r"\d", password) and
-        re.search(r"[^\w\s]", password)):
-        return True
-    return False
